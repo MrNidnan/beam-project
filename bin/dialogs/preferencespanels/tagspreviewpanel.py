615da820fdede5e7f48ba46c58ceba6ddcc4e6de
piotr_r_sidorowicz <prsidoro@gmail.com>
2023-05-05 19:46:40 -0400

    Small cleanup

--- bin/dialogs/preferencespanels/tagspreviewpanel.py
+++ bin/dialogs/preferencespanels/tagspreviewpanel.py
@@ -61,7 +61,6 @@ class TagsPreviewPanel(wx.Panel):
         self.TagDropdown.Bind(wx.EVT_COMBOBOX, self.DoRefresh)
 
         # List of tags
-        # self.TagsList = wx.ListBox(self, -1, size=wx.DefaultSize, choices=Tags, style= wx.LB_NEEDED_SB)
         self.TagsList = wx.ListCtrl(self, -1, size=wx.DefaultSize, style=wx.LC_REPORT)
         bgcolour = self.TagsList.GetBackgroundColour()
         logging.debug("... bgcolour: " + str(bgcolour))
@@ -111,21 +110,14 @@ class TagsPreviewPanel(wx.Panel):
             preTag = '%NextTanda'
         else:
             for i in range(0, len(additionalTags)):
-                try:
-                    index = self.TagsList.InsertItem(i, additionalTags[i])
-                    self.TagsList.SetItem(index, 1, self.nowPlayingDataModel.convDict[additionalTags[i]])
-                except:
-                    index = self.TagsList.InsertItem(i, additionalTags[i])
-                    self.TagsList.SetItem(index, 1, str(' '))
+                index = self.TagsList.InsertItem(i, additionalTags[i])
+                try: self.TagsList.SetItem(index, 1, self.nowPlayingDataModel.convDict[additionalTags[i]])
+                except: self.TagsList.SetItem(index, 1, str(' '))
 
         if not TagsSelected == 'Misc':
-            # try:
             for i in range(0, len(attributes)):
-                try:
-                    index = self.TagsList.InsertItem(i, preTag+attributes[i])
-                    self.TagsList.SetItem(index, 1, self.nowPlayingDataModel.convDict[preTag+attributes[i]])
-                except:
-                    index = self.TagsList.InsertItem(i, preTag+attributes[i])
-                    self.TagsList.SetItem(index, 1, str(' '))
+                index = self.TagsList.InsertItem(i, preTag+attributes[i])
+                try: self.TagsList.SetItem(index, 1, self.nowPlayingDataModel.convDict[preTag+attributes[i]])
+                except: self.TagsList.SetItem(index, 1, str(' '))
 
 
